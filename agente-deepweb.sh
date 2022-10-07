47a709fc46a8c356c6d05343099beeb62578869a
anderson84PROGETOS <115313235+anderson84PROGETOS@users.noreply.github.com>
2022-10-07 20:00:34 -0300

    Update agente-deepweb.sh

--- agente-deepweb.sh
+++ agente-deepweb.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-
 echo
 curl -A "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.106 Safari/537.36" -L -s $1 | grep -Poi 'http\K.*?(?=")' | awk -F/ '{print $3}' | sort -n | uniq
