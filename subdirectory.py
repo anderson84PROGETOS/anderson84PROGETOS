f32db37cfd82cc66609cbe9157e7b2c1033bc0a1
anderson84PROGETOS <115313235+anderson84PROGETOS@users.noreply.github.com>
2023-02-22 01:55:35 -0300

    Update subdirectory.py

--- subdirectory.py
+++ subdirectory.py
@@ -8,14 +8,10 @@ response = requests.get(url)
 soup = BeautifulSoup(response.text, "html.parser")
 
 for link in soup.find_all('a'):
-
     href = link.get('href')
-
     parsed_href = urlparse(href)
-
     if parsed_href.scheme in ['http', 'https']:
-
         print(href)
-
+        
 input("\nQuery Closed Press Enter Exit !\n")
 
