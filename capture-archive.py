441845bf4cb5ebfb05e20a844f94c6c6d85ebcb2
anderson84PROGETOS <115313235+anderson84PROGETOS@users.noreply.github.com>
2023-03-04 10:04:07 -0300

    Create capture-archive.py

---
+++ capture-archive.py
@@ -0,0 +1,42 @@
+import requests
+import json
+import argparse
+
+def wayback_machine(url, output_file=None):
+    # Construir a URL da API do Wayback Machine do Internet Archive
+    api_url = "http://web.archive.org/cdx/search/cdx?url=*.{}/*&output=json&fl=original&collapse=urlkey".format(url)
+
+    # Fazer uma solicitação GET à API
+    response = requests.get(api_url)
+
+    # Analisar a resposta JSON
+    if response.status_code == 200:
+        data = json.loads(response.text)
+
+        # Verificar se há capturas de tela do site
+        if len(data) > 1:
+            print("\n🔎 Capturas de tela disponíveis para o site 🔍\n")
+            urls = []
+            for item in data[1:]:
+                url = item[0]
+                urls.append(url)
+                print(url)
+            # Salvar as URLs em um arquivo de texto, se necessário
+            if output_file is not None:
+                with open(output_file, "w") as f:
+                    for url in urls:
+                        f.write(url + "\n")
+                print("\nAs URLs foram salvas no arquivo {}.".format(output_file))
+        else:
+            print("\nNenhuma captura de tela disponível para o site.")
+    else:
+        print("\/Não foi possível acessar o Wayback Machine do Internet Archive.")
+
+# Definir os argumentos da linha de comando
+parser = argparse.ArgumentParser()
+parser.add_argument("url", help="Digite o nome do site para verificar capturas de tela")
+parser.add_argument("-o", "--output", help="Nome do arquivo para salvar as URLs das capturas de tela")
+args = parser.parse_args()
+
+# Chamar a função wayback_machine
+wayback_machine(args.url, args.output)
