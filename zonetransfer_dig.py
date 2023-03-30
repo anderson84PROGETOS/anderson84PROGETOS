37b639f23f41f5ffa06bca8b59fc68d4545e6b88
anderson84PROGETOS <115313235+anderson84PROGETOS@users.noreply.github.com>
2023-03-29 22:08:48 -0300

    Update zonetransfer_dig.py

--- zonetransfer_dig.py
+++ zonetransfer_dig.py
@@ -1,28 +1,26 @@
 import subprocess
 
 # Prompt the user for a domain name
-domain_name = input('\nEnter a domain name: ')
+domain_name = input('\nDigite o nome do website que deseja consultar: ')
 
 # Perform 'dig' command for the first query
 query1 = subprocess.run(['dig', 'ns', domain_name, '+short'], capture_output=True, text=True)
 
 # Print the output of the first query
-print('Output of the first query:')
+print('\n↓↓ Servidores DNS ↓↓\n')
 print(query1.stdout)
 
 # Perform 'dig' command for the second query
 query2 = subprocess.run(['dig', 'ns', domain_name, '+short'], capture_output=True, text=True)
 
-# Print the output of the second query
-print('Output of the second query:')
-print(query2.stdout)
-
 # Prompt the user for a nameserver to use for the zone transfer
-nameserver = input('Enter a nameserver for the zone transfer (nsztm1.digi.ninja): ')
+nameserver = input('Insira um servidor de nomes para a transferência de zona: ')
 
 # Perform 'dig' command for the zone transfer query
 zone_transfer = subprocess.run(['dig', 'axfr', '@' + nameserver, domain_name], capture_output=True, text=True)
 
 # Print the output of the zone transfer query
-print('Output of the zone transfer query:')
+print('\n↓↓ Saída da consulta de transferência de zona ↓↓\n')
 print(zone_transfer.stdout)
+
+input("\nconsulta de transferência de zona terminada [ENTER]")
