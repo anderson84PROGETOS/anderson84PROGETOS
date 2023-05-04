61cfb9a0775699ca9e8e338f997a468091638a7f
anderson84PROGETOS <115313235+anderson84PROGETOS@users.noreply.github.com>
2023-05-04 10:14:55 -0300

    Update Hash de Favicon.py

--- Hash de Favicon.py
+++ Hash de Favicon.py
@@ -7,7 +7,6 @@ import webbrowser
 import pyperclip
 import tkinter as tk
 
-
 def get_favicon_urls(url):
     # Fazer uma requisição HTTP à página web
     response = requests.get(url)
@@ -36,8 +35,7 @@ def get_favicon_urls(url):
 
     # Exibir os hashes dos favicons na janela
     if len(favicon_hashes) > 0:
-        all_hashes = "".join([str(h) for h in favicon_hashes])  
-
+        all_hashes = "".join([str(h) for h in favicon_hashes])
 
 def get_hash(favicon_url):
     response = requests.get(favicon_url)
@@ -48,13 +46,11 @@ def get_hash(favicon_url):
     else:
         return f"\nNão foi possível obter o favicon de {favicon_url}"
 
-
 def open_shodan():
     hash_input = hash_entry.get()
     shodan_url = f"https://www.shodan.io/search?query=http.favicon.hash%3A{hash_input}"
     webbrowser.open(shodan_url)
     shodan_info_label.config(text="\nVocê foi redirecionado para a pesquisa do Shodan")
-
 
 def copy_results():
     result = favicon_info_label.cget("text")
@@ -63,13 +59,20 @@ def copy_results():
     results = f"{result}\n{shodan_link}"
     pyperclip.copy(results)
 
-
-
+# botao Limpar Resultados
+def clear_all():
+    favicon_info_label.config(text="")
+    shodan_link_label.config(text="")
+    shodan_info_label.config(text="")
+    hash_entry.delete(0, 'end')
+    url_entry.delete(0, 'end')
 
 window = tk.Tk()
 window.wm_state('zoomed')
 window.title("Favicon Hash Checker")
 
+info_text = tk.Text(window, font=("Arial", 10), width=50, height=25)
+info_text.place(x=400, y=450, width=870, height=500)
 
 url_label = tk.Label(window, text="Digite a URL da página", width=20)
 url_entry = tk.Entry(window, width=50)
@@ -83,8 +86,8 @@ open_shodan_button = tk.Button(window, text="Abrir Shodan", command=open_shodan)
 favicon_info_label = tk.Label(window, text="")
 shodan_link_label = tk.Label(window, text="")
 shodan_info_label = tk.Label(window, text="")
-copy_results_button = tk.Button(window, text="Copiar Resultados", command=copy_results)
 
+copy_results_button = tk.Button(window, text="Copiar Resultados", command=copy_results, bg="blue", fg="white")
 
 url_label.grid(row=0, column=0, padx=5, pady=5, sticky="w")
 url_entry.grid(row=0, column=1, padx=5, pady=5, sticky="w")
@@ -97,5 +100,9 @@ favicon_info_label.grid(row=2, column=0, columnspan=3, padx=5, pady=5, sticky="w
 shodan_link_label.grid(row=3, column=0, columnspan=3, padx=5, pady=5, sticky="w")
 shodan_info_label.grid(row=4, column=0, columnspan=3, padx=5, pady=5, sticky="w")
 copy_results_button.grid(row=5, column=0, columnspan=3, padx=5, pady=5, sticky="w")
+
+# botao Limpar Resultados
+clear_all_button = tk.Button(window, text="Limpar Resultados", command=clear_all, bg="red")
+clear_all_button.grid(row=6, column=0, columnspan=3, padx=5, pady=200, sticky="w")
 
 window.mainloop()
