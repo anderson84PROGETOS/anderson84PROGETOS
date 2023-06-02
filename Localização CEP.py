c8dabcf5223e082d730692711b03b4b7d84e8aac
anderson84PROGETOS <115313235+anderson84PROGETOS@users.noreply.github.com>
2023-06-02 17:57:03 -0300

    Update Localização CEP.py

--- Localização CEP.py
+++ Localização CEP.py
@@ -13,7 +13,7 @@ def get_address_info():
         endereco = response.json()
         uf = endereco['uf']
         regiao_url = f"https://servicodados.ibge.gov.br/api/v1/localidades/estados/{uf}"
-        regiao_response = requests.get(regiao_url)
+        regiao_response = requests.get(regiao_url, verify=False)
         if regiao_response.status_code == 200:
             regiao = regiao_response.json()['regiao']['nome']
         else:
@@ -35,36 +35,36 @@ def get_address_info():
             bairro_label.config(text="")
             cidade_label.config(text="")            
             latitude_label.config(text="")
-            longitude_label.config(text="impossível obter localização CEP")
+            longitude_label.config(text="Impossível obter localização do CEP")
             
     else:
         rua_label.config(text="")
         bairro_label.config(text="")
         cidade_label.config(text="")        
         latitude_label.config(text="")
-        longitude_label.config(text="impossível obter informações CEP ")        
+        longitude_label.config(text="Impossível obter informações do CEP")
 
 root = tk.Tk()
 root.title("Localização CEP")
 root.geometry('1000x800')
 root.configure(bg='dark blue')
 
-cep_label = tk.Label(root, text="Digite o CEP (somente números):", fg='white', bg='dark blue',font=("Arial Bold", 12))
+cep_label = tk.Label(root, text="Digite o CEP (somente números):", fg='white', bg='dark blue', font=("Arial Bold", 12))
 cep_label.grid(row=0, column=0, padx=5, pady=15, sticky='w')
 
-cep_entry = tk.Entry(root, width=40,font=("Arial Bold", 12))
+cep_entry = tk.Entry(root, width=40, font=("Arial Bold", 12))
 cep_entry.grid(row=0, column=1, padx=10, pady=10)
 
-consultar_button = tk.Button(root, text="Consultar", command=get_address_info,font=("Arial Bold", 12))
+consultar_button = tk.Button(root, text="Consultar", command=get_address_info, font=("Arial Bold", 12))
 consultar_button.grid(row=0, column=2, padx=5, pady=5)
 
-rua_label = tk.Label(root, text="", fg='white', bg='dark blue',font=("Arial Bold", 12))
+rua_label = tk.Label(root, text="", fg='white', bg='dark blue', font=("Arial Bold", 12))
 rua_label.grid(row=1, column=0, padx=5, pady=5, sticky='w')
 
-bairro_label = tk.Label(root, text="", fg='white', bg='dark blue',font=("Arial Bold", 12))
+bairro_label = tk.Label(root, text="", fg='white', bg='dark blue', font=("Arial Bold", 12))
 bairro_label.grid(row=2, column=0, padx=5, pady=5, sticky='w')
 
-cidade_label = tk.Label(root, text="", fg='white', bg='dark blue',font=("Arial Bold", 12))
+cidade_label = tk.Label(root, text="", fg='white', bg='dark blue', font=("Arial Bold", 12))
 cidade_label.grid(row=3, column=0, padx=5, pady=5, sticky='w')
 
 latitude_label = tk.Label(root, text="", fg='white', bg='dark blue',font=("Arial Bold", 12))
