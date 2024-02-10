6b31930c32e4f0649c31ebfd4ff099e3faa67aa3
anderson84PROGETOS <115313235+anderson84PROGETOS@users.noreply.github.com>
2024-02-10 00:51:12 -0300

    search subweb.py

---
+++ search subweb.py
@@ -0,0 +1,69 @@
+import requests
+import re
+import socket
+from urllib.parse import urlparse
+from io import BytesIO
+
+def get_main_site_ip(url):
+    parsed_url = urlparse(url)
+    main_site_domain = parsed_url.netloc
+    try:
+        ip_address = socket.gethostbyname(main_site_domain)
+    except socket.gaierror:
+        ip_address = 'Unknown'
+    return ip_address
+
+def search_subdomains(url):
+    if not url.startswith('http'):
+        url = f'http://{url}'
+    response = requests.get(url)
+    subdomains = set(re.findall(r'(https?://(?:[\w-]+\.)+[\w]+)', response.text))
+    num_subdomains = len(subdomains)
+    main_site_ip = get_main_site_ip(url)
+
+    subdomains_info = []
+
+    for subdomain in subdomains:
+        try:
+            ip_address = socket.gethostbyname(subdomain.split('//')[1])
+        except socket.gaierror:
+            ip_address = main_site_ip
+
+        subdomains_info.append((subdomain, ip_address))
+
+    return num_subdomains, subdomains_info
+
+def save_to_file(subdomains_info, file_path):
+    with open(file_path, 'w', encoding='utf-8') as file:
+        for subdomain, ip_address in subdomains_info:
+            file.write(f'{subdomain}   ===>   {ip_address}\n\n')
+
+print("""\n
+
+    ███████╗███████╗ █████╗ ██████╗  ██████╗██╗  ██╗    ███████╗██╗   ██╗██████╗ ██╗    ██╗███████╗██████╗ 
+    ██╔════╝██╔════╝██╔══██╗██╔══██╗██╔════╝██║  ██║    ██╔════╝██║   ██║██╔══██╗██║    ██║██╔════╝██╔══██╗
+    ███████╗█████╗  ███████║██████╔╝██║     ███████║    ███████╗██║   ██║██████╔╝██║ █╗ ██║█████╗  ██████╔╝
+    ╚════██║██╔══╝  ██╔══██║██╔══██╗██║     ██╔══██║    ╚════██║██║   ██║██╔══██╗██║███╗██║██╔══╝  ██╔══██╗
+    ███████║███████╗██║  ██║██║  ██║╚██████╗██║  ██║    ███████║╚██████╔╝██████╔╝╚███╔███╔╝███████╗██████╔╝
+    ╚══════╝╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝    ╚══════╝ ╚═════╝ ╚═════╝  ╚══╝╚══╝ ╚══════╝╚═════╝ 
+                                                                                                                                                                                                                                
+""")            
+
+def main():
+    url = input("\nDigite o Nome do site ou a URL do website: ")
+    num_subdomains, subdomains_info = search_subdomains(url)
+
+    print(f"\nForam encontrados {num_subdomains} subdomínios\n")
+    for subdomain, ip_address in subdomains_info:
+        print(f"{subdomain}   ===>   {ip_address}")
+
+    salvar = input("\nDeseja salvar as informações em um arquivo? (s/n): ").lower()
+    if salvar == "s":
+        save_to_file(subdomains_info, "subdomains.txt")
+        print("\n\nAs informações foram salvas no arquivo subdomains.txt")
+    else:
+        print("\n\nAs informações não foram salvas")
+
+if __name__ == "__main__":
+    main()
+input("\nFIM [APERTE ENTER PARA SAIR]\n")
