8617ad722c095ce383ae1cde1bb6c8f50094f6e6
anderson84PROGETOS <115313235+anderson84PROGETOS@users.noreply.github.com>
2024-06-11 17:45:31 -0300

    WebsiteAnalyzer.py

--- WebsiteAnalyzer.py
+++ WebsiteAnalyzer.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import scrolledtext
+from tkinter import scrolledtext, messagebox
 import urllib.request
 import urllib.error
 import socket
@@ -12,24 +12,26 @@ def analyze_and_check_website():
     url = website_entry.get().replace('http://', '').replace('https://', '').split('/')[0]
     try:
         response = urllib.request.urlopen("http://" + url, timeout=5)
-        result_text.insert(tk.END, '\nCabeçalhos HTTP do Response\n')
+        result_text.insert(tk.END, '============== Cabeçalhos HTTP =========================\n\n')
         result_text.insert(tk.END, response.headers)
         
-        # Verifica o status da conexão
-        status_code = response.getcode()
-        result_text.insert(tk.END, f'\nStatus da conexão: {status_code}\n')
-        if status_code == 200:
-            result_text.insert(tk.END, f'Conexão com o {url} estabelecida com sucesso!\n\n')
-        else:
-            result_text.insert(tk.END, f'Erro ao conectar-se ao {url}: {response.reason}\n')
+        # Verifica o status da conexão (comentado para não exibir)
+        # status_code = response.getcode()
+        # result_text.insert(tk.END, f'\nStatus da conexão: {status_code}')
+        # if status_code == 200:
+        #     result_text.insert(tk.END, f'\nConexão com o {url} estabelecida com sucesso!\n\n')
+        # else:
+        #     result_text.insert(tk.END, f'\nErro ao conectar-se ao {url}: {response.reason}')
     except urllib.error.HTTPError as e:
+        # Se a solicitação retornar um código de status 403 (Forbidden)
         if e.code == 403:
-            result_text.insert(tk.END, '\n============== Cabeçalhos HTTP =========================\n')
-            result_text.insert(tk.END, e.headers)            
+            result_text.insert(tk.END, '============== Cabeçalhos HTTP =========================\n\n')
+            result_text.insert(tk.END, e.headers)
         else:
-            result_text.insert(tk.END, f'Erro ao conectar-se ao {url}: {e}\n')
+            result_text.insert(tk.END, f'\nErro ao conectar-se ao {url}: {e}')
     except Exception as e:
-        result_text.insert(tk.END, f'Erro ao conectar-se ao {url}: {e}\n')
+        result_text.insert(tk.END, f'\nErro ao conectar-se ao {url}: {e}')
+
 
 def get_ipv4_addresses():
     site = website_entry.get().replace('http://', '').replace('https://', '').split('/')[0]
@@ -48,13 +50,12 @@ def get_ipv4_addresses():
         ipv4_addresses.extend(ipv4_matches)
 
     ipv4_addresses = list(set(ipv4_addresses))
-    
     if ipv4_addresses:
-        result_text.insert(tk.END, "\n============== Endereços IPv4 Encontrados ================\n")
+        result_text.insert(tk.END, "\n\n============== Endereços IPv4 Encontrados ================\n\n")
         for ipv4_address in ipv4_addresses:
-            result_text.insert(tk.END, ipv4_address + "\n")
+            result_text.insert(tk.END, ipv4_address + '\n')
     else:
-        result_text.insert(tk.END, "\nNenhum Endereço IPv4 encontrado.\n")
+        result_text.insert(tk.END, "\nNenhum Endereço IPv4 encontrado.")
 
 def get_geolocation_info():
     site = website_entry.get().replace('http://', '').replace('https://', '').split('/')[0]
@@ -64,13 +65,16 @@ def get_geolocation_info():
         if response.status_code == 200:
             data = response.json()
             if data['status'] == 'success':
-                result_text.insert(tk.END, "\n\n\n============== Informações de Geolocalização ==============\n")
-                result_text.insert(tk.END, f"Provedor de Internet: {data['isp']}\n")
+                result_text.insert(tk.END, "\n\n\n============== Informações de Geolocalização ==============\n\n")
+                result_text.insert(tk.END, f"\nProvedor de Internet: {data['isp']}\n\n")
                 result_text.insert(tk.END, f"País: {data['country']}\n")
-                result_text.insert(tk.END, f"Cidade: {data['city']}\n")        
+                result_text.insert(tk.END, f"Cidade: {data['city']}\n")
                 result_text.insert(tk.END, f"Latitude: {data['lat']}\n")
                 result_text.insert(tk.END, f"Longitude: {data['lon']}\n")
                 
+                # Gera e insere a URL do Google Maps
+                google_maps_url = f"https://www.google.com/maps/place/{data['lat']},{data['lon']}"
+                result_text.insert(tk.END, f"\nURL do Google Maps: {google_maps_url}\n")
                 return data['lat'], data['lon']
             else:
                 result_text.insert(tk.END, "Erro ao obter informações de geolocalização: status não é 'success'\n")
@@ -89,12 +93,13 @@ def obter_informacoes_website():
         if resposta.status_code == 200:
             dados = resposta.json()
             if dados['status'] == 'success':
-                result_text.insert(tk.END, "\n\n\n============== Informações sobre o website ================\n")                
+                result_text.insert(tk.END, "\n\n\n============== Informações sobre o website ================\n\n") 
+                result_text.insert(tk.END, f"\nTECNOLOGIAS: {dados['isp']}\n")               
                 result_text.insert(tk.END, f"\nIP: {dados['query']}\n")
                 result_text.insert(tk.END, f"\nPaís: {dados['country']}\n")
                 result_text.insert(tk.END, f"\nCidade: {dados['city']}\n")
-                result_text.insert(tk.END, f"\nProvedor de Internet: {dados['isp']}\n")
 
+                # Obtendo informações adicionais usando o Nominatim
                 if 'lat' in dados and 'lon' in dados:
                     lat = dados['lat']
                     lon = dados['lon']
@@ -118,11 +123,22 @@ def obter_informacoes_website():
         return None, None
 
 def open_google_maps():
-    lat, lon = get_geolocation_info()
-    if lat and lon:
-        google_maps_url = f"https://www.google.com/maps/place/{lat},{lon}"
-        result_text.insert(tk.END, f"\nURL do Google Maps: {google_maps_url}\n")
-        webbrowser.open(google_maps_url)
+    site = website_entry.get().replace('http://', '').replace('https://', '').split('/')[0]
+    try:
+        ip = socket.gethostbyname(site)
+        response = requests.get(f"http://ip-api.com/json/{ip}")
+        if response.status_code == 200:
+            data = response.json()
+            if data['status'] == 'success':
+                google_maps_url = f"https://www.google.com/maps/place/{data['lat']},{data['lon']}"
+                if messagebox.askyesno("Abrir Mapa", "Abrir o mapa do Google com a geolocalização?"):
+                    webbrowser.open(google_maps_url)
+            else:
+                messagebox.showerror("Erro", "Erro ao obter informações de geolocalização")
+        else:
+            messagebox.showerror("Erro", f"Erro ao obter informações de geolocalização: {response.status_code}")
+    except Exception as e:
+        messagebox.showerror("Erro", f"Erro ao obter informações de geolocalização: {e}")
 
 # Configurar a janela principal
 window = tk.Tk()
@@ -138,7 +154,7 @@ website_label = tk.Label(input_frame, text="Digite o nome do Website", font=("Ar
 website_label.grid(column=0, row=0)
 
 website_entry = tk.Entry(input_frame, width=30, font=("Arial", 12))
-website_entry.grid(column=0, row=1, padx=5, pady=5)
+website_entry.grid(column=0, row=1, padx=5, pady=2)
 
 # Adicionando um espaço vazio entre a entrada de texto e os botões
 empty_space = tk.Label(input_frame)
@@ -146,19 +162,19 @@ empty_space.grid(column=0, row=2)
 
 # Botão "Analisar Website"
 button_analyze = tk.Button(input_frame, text="Analisar Website Cabeçalhos HTTP", command=analyze_and_check_website)
-button_analyze.grid(column=0, row=3, pady=5)
+button_analyze.grid(column=0, row=3, pady=2)
 
 # Botão "Exibir Endereços IPv4"
 button_ipv4 = tk.Button(input_frame, text="Exibir Endereços IPv4", command=get_ipv4_addresses)
 button_ipv4.grid(column=0, row=4, pady=5)
 
-# Botão "Obter Geolocalização"
-button_geolocation = tk.Button(input_frame, text="Obter Geolocalização", command=get_geolocation_info)
-button_geolocation.grid(column=0, row=5, pady=5)
-
 # Botão "Obter Informações do Website"
 button_website_info = tk.Button(input_frame, text="Obter Informações do Website", command=obter_informacoes_website)
-button_website_info.grid(column=0, row=6, pady=5)
+button_website_info.grid(column=0, row=5, pady=5)
+
+# Botão "Obter Geolocalização"
+button_geolocation = tk.Button(input_frame, text="Obter Geolocalização", command=get_geolocation_info)
+button_geolocation.grid(column=0, row=6, pady=5)
 
 # Botão "Abrir Mapa no Google Maps"
 button_open_maps = tk.Button(input_frame, text="Abrir Mapa no Google Maps", command=open_google_maps, bg="#0bfc03")
@@ -173,10 +189,10 @@ input_frame.grid_rowconfigure(7, weight=1)
 
 # Criar o frame para os resultados
 result_frame = tk.Frame(window)
-result_frame.grid(column=0, row=7, columnspan=2, padx=5, pady=2)
+result_frame.grid(column=0, row=8, columnspan=2, padx=5, pady=2)
 
 # Criar o widget ScrolledText para os resultados
 result_text = scrolledtext.ScrolledText(result_frame, wrap=tk.WORD, width=135, height=36, font=("Arial", 12))
-result_text.grid(column=0, row=0, sticky=tk.W)
+result_text.grid(column=0, row=0, padx=30, sticky=tk.W)
 
 window.mainloop()
