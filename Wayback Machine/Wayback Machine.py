1f2dd23dd0bf2309995d4d6eb6cdbb1426f560d8
anderson84PROGETOS <115313235+anderson84PROGETOS@users.noreply.github.com>
2025-01-02 09:37:40 -0300

    Wayback Machine.py

--- Wayback Machine/Wayback Machine.py
+++ Wayback Machine/Wayback Machine.py
@@ -1,16 +1,18 @@
 import requests
 import json
 import threading
+from colorama import Fore, Style, init
 
-print("""\
+# Inicializando o colorama
+init(autoreset=True)
 
+print(Fore.LIGHTCYAN_EX + Style.BRIGHT + """
 ██╗    ██╗ █████╗ ██╗   ██╗██████╗  █████╗  ██████╗██╗  ██╗    ███╗   ███╗ █████╗  ██████╗██╗  ██╗██╗███╗   ██╗███████╗
 ██║    ██║██╔══██╗╚██╗ ██╔╝██╔══██╗██╔══██╗██╔════╝██║ ██╔╝    ████╗ ████║██╔══██╗██╔════╝██║  ██║██║████╗  ██║██╔════╝
 ██║ █╗ ██║███████║ ╚████╔╝ ██████╔╝███████║██║     █████╔╝     ██╔████╔██║███████║██║     ███████║██║██╔██╗ ██║█████╗  
 ██║███╗██║██╔══██║  ╚██╔╝  ██╔══██╗██╔══██║██║     ██╔═██╗     ██║╚██╔╝██║██╔══██║██║     ██╔══██║██║██║╚██╗██║██╔══╝  
 ╚███╔███╔╝██║  ██║   ██║   ██████╔╝██║  ██║╚██████╗██║  ██╗    ██║ ╚═╝ ██║██║  ██║╚██████╗██║  ██║██║██║ ╚████║███████╗
  ╚══╝╚══╝ ╚═╝  ╚═╝   ╚═╝   ╚═════╝ ╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝    ╚═╝     ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚═╝╚═╝  ╚═══╝╚══════╝
-
 """)
 
 captured_urls = set()  # Usar um conjunto para eliminar URLs duplicadas
@@ -18,7 +20,9 @@ captured_urls = set()  # Usar um conjunto para eliminar URLs duplicadas
 def search_wayback_machine(url):
     global captured_urls
     # Fazer uma solicitação HTTP para o Arquivo de Internet do Wayback Machine
-    response = requests.get("http://web.archive.org/cdx/search/cdx?url=*.{}/*&output=json&fl=original&collapse=urlkey".format(url))
+    response = requests.get(
+        f"http://web.archive.org/cdx/search/cdx?url=*.{url}/*&output=json&fl=original&collapse=urlkey"
+    )
 
     # Verificar se a solicitação foi bem-sucedida
     if response.status_code == 200:
@@ -30,33 +34,37 @@ def search_wayback_machine(url):
             print("\nNenhuma URL encontrada para o domínio fornecido.")
             return
 
-        print("\nForam capturadas as seguintes URL do Wayback Machine\n" + "="*52)
-
-        # Exibir e armazenar as URLs capturadas
+        # Capturar URLs
         for url in data:
             filtered_url = url[0].replace("original", "")  # Filtrar a palavra "original"
-            print(f"\n{filtered_url}")  # Exibir a URL com uma linha em branco antes
             captured_urls.add(filtered_url)  # Adicionar ao conjunto para evitar duplicatas
 
-        print("\n\nForam capturadas: {} URL".format(len(captured_urls)))
+        # Exibir as URLs
+        print(Fore.LIGHTRED_EX + f"\n\nForam capturadas as seguintes URL do Wayback Machine\n" + "="*52)
+        for filtered_url in captured_urls:
+            print(f"{filtered_url}")  # Exibir a URL
+        print(Fore.LIGHTGREEN_EX + f"\n\nForam capturadas: {len(captured_urls)} URL")
     else:
-        print("\nErro: Não foi possível capturar as URL. Código de status do servidor: {}.".format(response.status_code))
+        print(f"\nErro: Não foi possível capturar as URL. Código de status do servidor: {response.status_code}.")
 
 def save_urls_to_file(urls_content):
     # Pede ao usuário para escolher o nome do arquivo
-    file_path = input("\nDigite o nome do arquivo para salvar as URL (ex: arquivo.txt): ")    
+    file_path = input(Fore.LIGHTCYAN_EX + Style.BRIGHT + "\nDigite o nome do arquivo para salvar as URL (ex: arquivo.txt): ")
 
     # Tenta salvar o conteúdo no arquivo
     try:
         with open(file_path, "w", encoding="utf-8") as file:
-            file.write("\n\n".join(urls_content).strip())  # Gravar as URLs com uma linha em branco entre elas
-        print("\nSucesso: As URL foram salvas com sucesso Em: {}".format(file_path))
+            # Escrever a quantidade de URLs encontradas
+            file.write(f"Total de URL Encontradas: {len(urls_content)}\n\n")
+            # Gravar as URLs em linhas separadas
+            file.write("\n".join(urls_content).strip())  
+        print(Fore.LIGHTCYAN_EX + Style.BRIGHT + f"\nSucesso: As URL foram salvas com sucesso em: {file_path}")
     except Exception as e:
-        print("Erro ao salvar o arquivo: {}".format(str(e)))
+        print(f"Erro ao salvar o arquivo: {str(e)}")
 
 def start_search():
-    url = input("\nDigite a URL que deseja procurar no Wayback Machine: ")
-    search_wayback_machine(url)
+    url = input(Fore.LIGHTGREEN_EX + "\nDigite a URL que deseja procurar no Wayback Machine: ")    
+    search_wayback_machine(url)    
 
 if __name__ == "__main__":
     # Iniciar a busca em uma thread separada
@@ -67,15 +75,17 @@ if __name__ == "__main__":
     # Após a busca, oferecer a opção de salvar as URLs
     if captured_urls:  # Verificar se as URLs foram capturadas
         while True:
-            save_option = input("\nDeseja salvar as URL em um arquivo? (s/n): ").lower()
+            save_option = input(Fore.LIGHTCYAN_EX + Style.BRIGHT + "\nDeseja salvar as URL em um arquivo? (s/n): ").lower()
             if save_option == 's':
                 save_urls_to_file(captured_urls)  # Usa as URLs capturadas anteriormente
                 break
-            elif save_option == 'n':
+            elif save_option == '':
+                print(Fore.LIGHTRED_EX + "\nAs URL não foram salvas.")
                 break
-            else:
-                print("\nOpção inválida. Por favor, digite 's' para sim ou 'n' para não.")
+            elif save_option == 'n':
+                print(Fore.LIGHTRED_EX + "\nAs URL não foram salvas.")
+                break
     else:
         print("\nErro: Nenhuma URL foi capturada para salvar.")
 
-input("\n\nPRESSIONE ENTER PARA SAIR\n=========================\n\n")
+input(Fore.LIGHTMAGENTA_EX + "\n\nPRESSIONE ENTER PARA SAIR\n=========================\n\n")
