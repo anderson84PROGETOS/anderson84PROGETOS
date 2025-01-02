89dd85ef9dbcccd84177192a6edbf40959d782d8
anderson84PROGETOS <115313235+anderson84PROGETOS@users.noreply.github.com>
2025-01-01 23:11:02 -0300

    whois block.py

--- whois block/whois block.py
+++ whois block/whois block.py
@@ -4,16 +4,18 @@ import requests
 import ipaddress  # Importa o módulo ipaddress
 from bs4 import BeautifulSoup
 from socket import *
+from colorama import Fore, Style, init
 
-print("""
+# Inicializando o colorama
+init(autoreset=True)
 
+print(Fore.LIGHTCYAN_EX + Style.BRIGHT + """
     ██╗    ██╗██╗  ██╗ ██████╗ ██╗███████╗    ██████╗ ██╗      ██████╗  ██████╗██╗  ██╗
     ██║    ██║██║  ██║██╔═══██╗██║██╔════╝    ██╔══██╗██║     ██╔═══██╗██╔════╝██║ ██╔╝
     ██║ █╗ ██║███████║██║   ██║██║███████╗    ██████╔╝██║     ██║   ██║██║     █████╔╝ 
     ██║███╗██║██╔══██║██║   ██║██║╚════██║    ██╔══██╗██║     ██║   ██║██║     ██╔═██╗ 
     ╚███╔███╔╝██║  ██║╚██████╔╝██║███████║    ██████╔╝███████╗╚██████╔╝╚██████╗██║  ██╗
      ╚══╝╚══╝ ╚═╝  ╚═╝ ╚═════╝ ╚═╝╚══════╝    ╚═════╝ ╚══════╝ ╚═════╝  ╚═════╝╚═╝  ╚═╝
-                                                                                    
 """)
 
 servidores_whois_tdl = {
@@ -70,7 +72,7 @@ def obter_whois_br(endereco):
     # Filtra linhas que não começam com '%'
     linhas_filtradas = [linha for linha in resultado.splitlines() if not linha.startswith('%')]
     resultado_filtrado = '\n'.join(linhas_filtradas)
-    print(resultado_filtrado)
+    print(Fore.LIGHTGREEN_EX + resultado_filtrado)
 
 def consulta_whois(endereco):
     obter_whois_br(endereco)
@@ -79,32 +81,32 @@ def list_subnet_ips(subnet):
     try:
         network = ipaddress.ip_network(subnet)
     except ValueError:
-        print("\nErro: Insira um bloco de IP válido. Exemplo: 200.196.144.0/20\n")
+        print(Fore.LIGHTRED_EX + "\nErro: Insira um bloco de IP válido. Exemplo: 200.196.144.0/20\n")
         return
 
-    print("\nEndereços IP disponíveis na sub-rede\n")
+    print(Fore.LIGHTGREEN_EX + "\nEndereços IP disponíveis na sub-rede\n")
     ips = []
 
     for ip in network.hosts():
         ips.append(str(ip))
-        print(ip)
+        print(Fore.LIGHTYELLOW_EX + str(ip))
 
-    save = input("\nDeseja salvar os IP em um arquivo? (s/n): ")
+    save = input(Fore.LIGHTMAGENTA_EX + "\nDeseja salvar os IP em um arquivo? (s/n): ")
     if save.lower() == 's':
-        filename = input("\nDigite o nome do arquivo para salvar os IP: ")
+        filename = input(Fore.LIGHTCYAN_EX + "\nDigite o nome do arquivo para salvar os IP: ")
         try:
             with open(filename, 'w') as f:
                 for ip in ips:
                     f.write(ip + '\n')
-            print(f"\nIP salvos no arquivo: {filename}")
+            print(Fore.LIGHTCYAN_EX + f"\nIP salvos no arquivo: {filename}")
         except Exception as e:
-            print(f"\nErro ao salvar os IP no arquivo: {e}\n")
+            print(Fore.LIGHTRED_EX + f"\nErro ao salvar os IP no arquivo: {e}\n")
 
-def main():
-    domain = input("Digite o nome do Website: ")
+def main():    
+    domain = input(Fore.LIGHTCYAN_EX + f"\nDigite o nome do Website: ")
     print()    
     mx_records = get_mx_records(domain)   
-    print(f"\nRegistros MX para website: {domain}\n")
+    print(Fore.LIGHTRED_EX + f"\nRegistros MX para website: {domain}\n")
     
     for line in mx_records.splitlines():
         if "mail exchanger" in line:
@@ -113,26 +115,26 @@ def main():
             # Exibindo o domínio seguido do registro MX
             ping_result = ping_host(mx_host)
             ip_address = obter_ip_do_ping(ping_result)
-            print(f"{domain}     MX = {mx_host}    IP: {ip_address}")
+            print(Fore.LIGHTMAGENTA_EX + f"{domain}     MX = {mx_host}    IP: {ip_address}")
     
     for line in mx_records.splitlines():
         if "mail exchanger" in line:
             mx_host = line.split('=')[-1].strip()
-            print(f"\n\nPinging no website: {mx_host}\n")
+            print(Fore.LIGHTRED_EX + f"\n\nPinging no website: {mx_host}\n")
             ping_result = ping_host(mx_host)
 
             # Extrai apenas o endereço IP do resultado do ping
             ip_address = obter_ip_do_ping(ping_result)
             if ip_address:
-                print(f"Consultando WHOIS para IP: {ip_address}")
+                print(Fore.LIGHTMAGENTA_EX + f"Consultando WHOIS para IP: {ip_address}")
                 consulta_whois(ip_address)
             else:
-                print(f"\nNão foi possível obter o IP do host: {mx_host}\n")
+                print(Fore.LIGHTRED_EX + f"\nNão foi possível obter o IP do host: {mx_host}\n")
 
-    subnet = input("\nDigite o bloco de IP do inetnum: (exemplo: 200.196.144.0/20): ")
+    subnet = input(Fore.LIGHTMAGENTA_EX + "\nDigite o bloco de IP do inetnum: (exemplo: 200.196.144.0/20): ")
     list_subnet_ips(subnet)
 
 if __name__ == "__main__":
     main()
 
-input("\n\nPRESSIONE ENTER PARA SAIR\n=========================\n")
+input(Fore.LIGHTBLUE_EX + Style.BRIGHT + "\n\n========== PRESSIONE ENTER PARA SAIR ==========\n")
