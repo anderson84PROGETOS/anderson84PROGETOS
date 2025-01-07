dd78af780ef9efecd2f6919e95dff522b2a47471
anderson84PROGETOS <115313235+anderson84PROGETOS@users.noreply.github.com>
2025-01-07 20:31:52 -0300

    Whois Check.py

---
+++ Whois Check/Whois Check.py
@@ -0,0 +1,244 @@
+import re
+import requests
+from bs4 import BeautifulSoup
+from socket import *
+import dns.resolver
+from colorama import Fore, Style, init
+
+# Inicializando o colorama
+init(autoreset=True)
+
+print(Fore.LIGHTCYAN_EX + Style.BRIGHT + """
+
+██╗    ██╗██╗  ██╗ ██████╗ ██╗███████╗     ██████╗██╗  ██╗███████╗ ██████╗██╗  ██╗
+██║    ██║██║  ██║██╔═══██╗██║██╔════╝    ██╔════╝██║  ██║██╔════╝██╔════╝██║ ██╔╝
+██║ █╗ ██║███████║██║   ██║██║███████╗    ██║     ███████║█████╗  ██║     █████╔╝ 
+██║███╗██║██╔══██║██║   ██║██║╚════██║    ██║     ██╔══██║██╔══╝  ██║     ██╔═██╗ 
+╚███╔███╔╝██║  ██║╚██████╔╝██║███████║    ╚██████╗██║  ██║███████╗╚██████╗██║  ██╗
+ ╚══╝╚══╝ ╚═╝  ╚═╝ ╚═════╝ ╚═╝╚══════╝     ╚═════╝╚═╝  ╚═╝╚══════╝ ╚═════╝╚═╝  ╚═╝
+                                                                                                                                                                  
+""")
+
+servidores_whois_tdl = {
+    '.com': 'whois.verisign-grs.com',
+    '.net': 'whois.verisign-grs.com',
+    '.edu': 'whois.educause.edu',
+    '.br': 'whois.registro.br',
+    '.gov': 'whois.nic.gov',
+}
+
+def remover_copyright(texto):
+    # Remove o texto de copyright
+    copyright_pattern = re.compile(r"%.*\n", re.MULTILINE)
+    return re.sub(copyright_pattern, "", texto)
+
+def requisicao_whois(servidor_whois, endereco_host, padrao):
+    objeto_socket = socket(AF_INET, SOCK_STREAM)
+    conexao = objeto_socket.connect_ex((servidor_whois, 43))
+    resultado = ''
+    if conexao == 0:
+        if padrao:
+            if servidor_whois == 'whois.verisign-grs.com':  # Para domínios .com e .net
+                objeto_socket.send(f'domain {endereco_host}\r\n'.encode())
+            else:
+                objeto_socket.send(f'n + {endereco_host}\r\n'.encode())
+        else:
+            objeto_socket.send(f'{endereco_host}\r\n'.encode())
+        while True:
+            dados = objeto_socket.recv(65500)
+            if not dados:
+                break
+            resultado += dados.decode('latin-1')
+
+    objeto_socket.close()
+
+    # Filtra a mensagem indesejada "No match found" do WHOIS
+    if "No match found" in resultado:
+        return ''  # Retorna uma string vazia caso não encontre nenhum resultado
+
+    return resultado
+
+def encontrar_emails(soup):
+    email_regex = r"[\w\.-]+@[\w\.-]+"
+    emails = []
+
+    # Procura e retorna os e-mails na página principal do WHOIS
+    email_section = soup.find("div", class_="row-fluid registry-data")
+    if email_section:
+        email_text = email_section.find_all("div", class_="row")[1].find("div", class_="span9").get_text()
+        email_matches = re.findall(email_regex, email_text)
+        emails.extend(email_matches)
+
+    # Procura e retorna os e-mails no resultado completo do WHOIS
+    whois_section = soup.find("pre", class_="df-raw")
+    if whois_section:
+        whois_text = whois_section.get_text()
+        email_matches = re.findall(email_regex, whois_text)
+        emails.extend(email_matches)
+
+    return emails
+
+def extrair_campo(whois_section, label):
+    field = whois_section.find("div", string=re.compile(label))
+    if field:
+        value = field.find_next_sibling("div").get_text(strip=True)
+        return value
+    return ""
+
+def remover_informacoes_extra(texto):
+    # Remover informações extras, como o aviso de ICANN, outros termos e o link da ICANN
+    extra_pattern = re.compile(r"(URL of the ICANN WHOIS Data Problem Reporting System:.*|Last update of WHOIS database:.*|By submitting a query to the Amazon Registrar.*|Visit Amazon Registrar, Inc. at https://registrar.amazon.com.*|Contact information available here:.*|© \d{4}, Amazon\.com, Inc\..*|For more information on Whois status codes, please visit https://icann.org/epp|agree to abide by the following terms.*?https://docs.aws.amazon.com/Route53/latest/DeveloperGuide/domain-contact-support.html)", re.MULTILINE | re.IGNORECASE | re.DOTALL)
+    return re.sub(extra_pattern, "", texto)
+
+def obter_whois_com(endereco):
+    url_whois = f"https://www.whois.com/whois/{endereco}"
+    
+    # Realiza a requisição para obter as informações WHOIS
+    response_whois = requests.get(url_whois)
+
+    if response_whois.status_code == 200:
+        soup_whois = BeautifulSoup(response_whois.text, "html.parser")
+        whois_section = soup_whois.find("pre", class_="df-raw")
+        if whois_section:
+            whois_text = whois_section.get_text()
+            # Remover copyrights e informações extras do WHOIS
+            whois_text = remover_copyright(whois_text)
+            whois_text = remover_informacoes_extra(whois_text)
+            print(Fore.LIGHTYELLOW_EX + whois_text)
+
+            # Extraindo e exibindo mais informações
+            emails = encontrar_emails(soup_whois)
+            if emails:
+                print("\nE-mails encontrados:")
+                for email in emails:
+                    print(email)
+
+            # Extraindo mais campos, se necessário
+            name = extrair_campo(whois_section, "Registrant Name:")
+            registration_date = extrair_campo(whois_section, "Creation Date:")
+            expiration_date = extrair_campo(whois_section, "Registrar Registration Expiration Date:")
+
+            if name:
+                print(f"Nome do Titular: {name}")
+            if registration_date:
+                print(f"Data de Registro: {registration_date}")
+            if expiration_date:
+                print(f"Data de Expiração: {expiration_date}")            
+    else:
+        print("Erro ao obter informações WHOIS do site whois.com.")
+
+def obter_whois(endereco):
+    url_whois = f"https://www.whois.com/whois/{endereco}"
+    url_registro_br = f"https://registro.br/cgi-bin/whois/?qr={endereco}"
+
+    response_whois = requests.get(url_whois)
+    response_registro_br = requests.get(url_registro_br)
+
+    if response_whois.status_code == 200 and response_registro_br.status_code == 200:
+        if re.search(r'\.br$', endereco):
+            # Parse REGISTRO.BR
+            soup_registro_br = BeautifulSoup(response_registro_br.text, "html.parser")
+            div_result = soup_registro_br.find("div", class_="result")
+            if div_result:
+                result_text = div_result.get_text()
+                print(result_text)
+
+        elif re.search(r'\.com$', endereco):
+            # Parse WHOIS.COM
+            soup_whois = BeautifulSoup(response_whois.text, "html.parser")
+            whois_section = soup_whois.find("pre", class_="df-raw")
+            if whois_section:
+                whois_text = whois_section.get_text()
+                # Remover copyrights e informações extra do WHOIS
+                whois_text = remover_copyright(whois_text)
+                whois_text = remover_informacoes_extra(whois_text)
+                print(Fore.LIGHTMAGENTA_EX + whois_text)
+
+                # Extraindo mais informações
+                emails = encontrar_emails(soup_whois)
+                if emails:
+                    print("\nE-mails encontrados:")
+                    for email in emails:
+                        print(email)
+
+                # Extraindo mais campos se necessário
+                name = extrair_campo(whois_section, "Registrant Name:")
+                registration_date = extrair_campo(whois_section, "Creation Date:")
+                expiration_date = extrair_campo(whois_section, "Registrar Registration Expiration Date:")
+
+                if name:
+                    print(f"Nome do Titular: {name}")
+                if registration_date:
+                    print(f"Data de Registro: {registration_date}")
+                if expiration_date:
+                    print(f"Data de Expiração: {expiration_date}")            
+    else:
+        print("Erro ao obter informações WHOIS.")
+
+def obter_whois_br(endereco):
+    servidor_whois = servidores_whois_tdl['.br']
+    resultado = requisicao_whois(servidor_whois, endereco, False)
+    if resultado.strip():  # Verifica se a resposta não está vazia
+        print(Fore.LIGHTGREEN_EX + remover_copyright(resultado))
+    else:
+        print(Fore.LIGHTRED_EX + "Nenhuma informação encontrada para o domínio.")
+
+
+def obter_whois_gov(endereco):
+    servidor_whois_gov = servidores_whois_tdl.get('.gov', None)
+    if servidor_whois_gov:
+        resultado = requisicao_whois(servidor_whois_gov, endereco, False)
+        print(Fore.LIGHTMAGENTA_EX + remover_copyright(resultado))
+    else:
+        print("Servidor WHOIS para domínios .gov não encontrado.")
+
+def get_whois_info(ip_address):
+    url = f"https://who.is/whois-ip/ip-address/{ip_address}"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        # Parsear o conteúdo da página com BeautifulSoup
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        # Procurar pela tag <pre> que contém as informações WHOIS
+        whois_section = soup.find("pre")
+        
+        # Se a tag <pre> for encontrada, pegar o conteúdo
+        if whois_section:
+            whois_text = whois_section.get_text()
+
+            # Exibir apenas as informações WHOIS, se encontradas
+            if "No match found" not in whois_text:
+                print(Fore.LIGHTCYAN_EX + whois_text)       
+
+def obter_mx(endereco):
+    try:
+        respostas = dns.resolver.resolve(endereco, 'MX')
+        print(Fore.LIGHTMAGENTA_EX + "\n========== REGISTROS MX ENCONTRADOS ==========\n")
+        for resposta in respostas:
+            mx_host = str(resposta.exchange).rstrip('.')
+            try:
+                ip = gethostbyname(mx_host)
+                print(Fore.LIGHTMAGENTA_EX + f"\n{mx_host} IP: {ip}")
+            except:
+                print(f"{mx_host} IP: Não foi possível resolver")
+    except:
+        pass  # Nenhum erro ou registro será exibido caso não haja resultados.
+
+def consulta_whois():
+    endereco = input(Fore.LIGHTMAGENTA_EX + "Digite o IP ou Nome do website Para Consultar WHOIS: ").strip()
+    obter_whois_br(endereco)
+    if re.search(r'\.gov$', endereco):
+        obter_whois_gov(endereco)
+    obter_whois(endereco)
+    obter_whois_com(endereco)  # Adicionando a nova função aqui
+    obter_mx(endereco)       
+
+    whois_info = get_whois_info(endereco)
+    if whois_info:  # Verifica se a variável contém algo antes de imprimir
+        print(Fore.LIGHTCYAN_EX + whois_info)    
+
+# Executa a consulta WHOIS
+consulta_whois()
+
+input(Fore.LIGHTRED_EX + "\n\n========== PRESSIONE ENTER PARA SAIR ==========" + "\n\n")
