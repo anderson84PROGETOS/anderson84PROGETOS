75231cc4978a98cac1638d79fd9313dd3ad36e65
anderson84PROGETOS <115313235+anderson84PROGETOS@users.noreply.github.com>
2025-02-10 19:30:55 -0300

    Hidra Scan.py

--- Hidra Scan/Hidra Scan.py
+++ Hidra Scan/Hidra Scan.py
@@ -4,7 +4,6 @@ from colorama import init, Fore, Style
 
 # Inicializando o colorama
 init(autoreset=True)
-
 # Banner do script
 print(Fore.LIGHTCYAN_EX + Style.BRIGHT + """
 ██╗  ██╗██╗   ██╗██████╗ ██████╗  █████╗     ███████╗ ██████╗ █████╗ ███╗   ██╗
