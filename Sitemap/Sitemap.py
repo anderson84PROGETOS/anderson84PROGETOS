d2a38018a8b85d1807f1eda5d0204481bdffb151
anderson84PROGETOS <115313235+anderson84PROGETOS@users.noreply.github.com>
2025-02-25 23:44:36 -0300

    Sitemap.py

--- Sitemap/Sitemap.py
+++ Sitemap/Sitemap.py
@@ -5,16 +5,15 @@ from colorama import init, Fore, Style
 
 # Inicializando o colorama
 init(autoreset=True)
+
 # Banner do script
 print(Fore.LIGHTCYAN_EX + Style.BRIGHT + """
-
 ███████╗██╗████████╗███████╗███╗   ███╗ █████╗ ██████╗ 
 ██╔════╝██║╚══██╔══╝██╔════╝████╗ ████║██╔══██╗██╔══██╗
 ███████╗██║   ██║   █████╗  ██╔████╔██║███████║██████╔╝
 ╚════██║██║   ██║   ██╔══╝  ██║╚██╔╝██║██╔══██║██╔═══╝ 
 ███████║██║   ██║   ███████╗██║ ╚═╝ ██║██║  ██║██║     
 ╚══════╝╚═╝   ╚═╝   ╚══════╝╚═╝     ╚═╝╚═╝  ╚═╝╚═╝     
-                                                     
 """)
 
 def get_sitemap(url):
@@ -25,24 +24,26 @@ def get_sitemap(url):
         'Upgrade-Insecure-Requests': '1',
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8'
     }
-    
+
     sitemap_url = urljoin(url, '/sitemap.xml')
     try:
         response = requests.get(sitemap_url, headers=headers, timeout=10)
         if response.status_code == 200:
             urls = re.findall(r'<loc>(.*?)</loc>', response.text)
             if urls:
-                print(Fore.LIGHTMAGENTA_EX + Style.BRIGHT + f"\n\nForam Encontradas: {len(urls)} URL no sitemap\n")
-                for u in urls:
-                    print(Fore.LIGHTGREEN_EX + Style.BRIGHT + u)
-                
+                print(Fore.LIGHTMAGENTA_EX + Style.BRIGHT + f"\n\nForam encontradas: {len(urls)} URLs no sitemap\n")
+                global_count = 0
+                for idx, u in enumerate(urls, start=1):
+                    global_count += 1
+                    print(Fore.LIGHTGREEN_EX + Style.BRIGHT + f"\n{idx:<4}  =  {u}")
+
                 # Perguntar se o usuário deseja salvar os resultados
                 save_choice = input(Fore.LIGHTYELLOW_EX + Style.BRIGHT + "\n\nDeseja salvar os resultados (s/n): ").strip().lower()
                 if save_choice == 's':
                     file_name = input(Fore.LIGHTYELLOW_EX + Style.BRIGHT + "\nDigite o nome do arquivo para salvar (exemplo: arquivo.txt): ").strip()
                     with open(file_name, 'w') as f:
-                        for u in urls:
-                            f.write(u + '\n')
+                        for idx, u in enumerate(urls, start=1):
+                            f.write(f"{idx:<4}  =  {u}\n")
                     print(Fore.LIGHTYELLOW_EX + Style.BRIGHT + f"\nURL salvas com sucesso no arquivo: {file_name}")
             else:
                 print(Fore.LIGHTRED_EX + Style.BRIGHT + "\nNenhuma URL encontrada no sitemap.")
