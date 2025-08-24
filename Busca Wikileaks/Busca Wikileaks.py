13541cb80898b2efdb60d447640a5cd520cf44c1
anderson84PROGETOS <115313235+anderson84PROGETOS@users.noreply.github.com>
2025-08-24 14:01:10 -0300

    Busca Wikileaks.py

--- Busca Wikileaks/Busca Wikileaks.py
+++ Busca Wikileaks/Busca Wikileaks.py
@@ -117,7 +117,7 @@ def abrir_url_no_chrome_anonima(event):
 
 # --- GUI ---
 root = tk.Tk()
-root.title("Buscar Wikileaks")
+root.title("Busca Wikileaks")
 root.geometry("1250x885")
 
 frame = tk.Frame(root)
